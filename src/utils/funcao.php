4bb1725fb1ca49bea40567749b5bacedc2fde64e
LuckyBressan <lucas.a.dos.s.bressan@gmail.com>
2025-11-10 20:40:29 -0300

    feat: added function for fetching and decoding JSON data;

--- src/utils/funcao.php
+++ src/utils/funcao.php
@@ -4,3 +4,10 @@ function isEmpty($info)
 {
     return empty($info) || $info == '' || $info == null || (is_array($info) && count($info) == 0);
 }
+
+function getDadosPostJson(): array
+{
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
+    return is_array($data) ? $data : [];
+}
