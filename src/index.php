b8b5ee64b92287bb14eab9a3b0bee649c64bdcbf
LuckyBressan <lucas.a.dos.s.bressan@gmail.com>
2025-11-13 00:59:15 -0300

    feat: implement CORS policy;

--- src/index.php
+++ src/index.php
@@ -1,7 +1,21 @@
 <?php
 
-header('Content-Type: application/json; charset=utf-8');
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+} else {
+    header('Access-Control-Allow-Origin: *');
+}
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Credentials: true');
 
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
+header('Content-Type: application/json; charset=utf-8');
 
 require_once('autoload.php');
 require_once(__DIR__ . '/utils/funcao.php');
